30ad44f9ac514bed0a1277199a0cd392418e506e
Steve Bachta <sbachta90@gmail.com>
2023-03-24 18:53:01 -0400

    Initial commit

---
+++ src/main/java/teddy/food/teddyfood/TeddyFoodApplication.java
@@ -0,0 +1,13 @@
+package teddy.food.teddyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeddyFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeddyFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/teddy/food/teddyfood/TeddyFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package teddy.food.teddyfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeddyFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
